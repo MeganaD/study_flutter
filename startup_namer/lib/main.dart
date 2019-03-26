0d6eef0bddc801c2dc03f1759881e08d3e32dcf5
MeganaD <meganad@outlook.kr>
2019-03-26 17:59:18 +0900

    Write your first app : step3
    https://flutter.dev/docs/get-started/codelab#step-3-add-a-stateful-widget

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,22 +1,36 @@
+// Add a stateful widget
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body: new Center(
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
